1fad19f6f0e23e4e07f378797c70e1af90827723
Marco Veronese Passarella <marco.veronese.passarella@gmail.com>
2024-08-13 19:05:10 +0200

    Update stima_consumi.R

--- stima_consumi.R
+++ stima_consumi.R
@@ -6,30 +6,26 @@
 # Una volta scaricate le due serie, le salviamo (in Excel) con l'estensione csv
 
 # Ora carichiamo i dati dal file csv (in questo caso, dal mio Dropbox) ####
-data <- read.csv("https://www.dropbox.com/scl/fi/l4753ks3g6xn633hy6qhf/Dati_Italia.csv?rlkey=hn6t1vi1052nqj7t3j643avxx&st=8sw8pucy&dl=1")
+data <- read.csv("https://www.dropbox.com/scl/fi/lejngqmu81m0fhe3skubw/Dati_Italia_diff.csv?rlkey=kj6js628wluwhutobve3zim5b&st=latfeg2p&dl=1")
+
+# Ridenominiamo le variabili ####
+delta_C <- data$delta_C   # Variazione annua del consumo reale - variazione media dal 2002
+delta_Yd <- data$delta_Yd # variazione annua del reddito disponibile - variazione media dal 2002
 
 # Visualizziamo le due variabili di interesse ####
-plot(data$Anno, data$Ydt, type="l", col="blue",
-     main = "a) Reddito disponibile e consumi in Italia (2021-2023, prezzi costanti)",
-     ylab = "Milioni di euro", xlab = "Anno", ylim=range(min(data$Ct),max(data$Ydt)))
-lines(data$Anno, data$Ct, col="violet")
+par(mar = c(6, 6, 4, 2))
+plot(data$Anno, data$delta_Yd/1000, type="l", col="blue",
+     main = "a) Variazioni di reddito disponibile e consumo in Italia (2002-2023)",
+     ylab = "Variazioni in miliardi di euro (2010)", xlab = "Anno",
+     ylim=range(min(delta_C/1000),max(delta_Yd/1000)))
+lines(data$Anno, data$delta_C/1000, col="violet")
+abline(h=0)
+abline(v=2010)
 
-# Nota: le grandezze sono espresse in termini "reali". L'anno base utilizzato è il 2010 (con prezzi concatenati).
+# Nota: le grandezze sono espresse in termini "reali". L'anno base utilizzato Ã¨ il 2010 (con prezzi concatenati).
 
-# Definiamo e ridenominiamo le variabili da utilizzare nella regressione ####
-C <- log(data$Ct)   # Logaritmo naturale del consumo reale
-Yd <- log(data$Ydt) # Logaritmo naturale del reddito disponibile
-
-# Nota: prendiamo i logaritmi per stabilizzare la varianza delle serie (in gergo, ridurre l'eteroschedasticità)
-
-# Visualizziamo nuovamente le due variabili ####
-plot(data$Anno, Yd, type="l", col="blue2",
-     main = "b) Reddito disponibile e consumi in Italia (2021-2023, prezzi costanti)",
-     ylab = "Log", xlab = "Anno", ylim=range(min(C),max(Yd)))
-lines(data$Anno, C, col="violet")
-
-# Facciamo la regressione utilizzando un semplice modello lineare: C = c0 + c1*Yd) ####
-model <- lm(C ~ Yd, data = data)
+# Facciamo la regressione utilizzando un semplice modello lineare: delta(C) = c0 + c1*delta(Yd) ####
+model <- lm(delta_C ~ delta_Yd, data = data)
 
 # Verifichiamo i risultati della regressione ####
 summary(model)
@@ -37,19 +33,21 @@ summary(model)
 # Visualizziamo e salviamo i coefficienti del modello (c0 e c1) ####
 print(model$coefficients)
 c0 <- coef(model)["(Intercept)"]
-c1 <- coef(model)["Yd"]
+c1 <- coef(model)["delta_Yd"]
 
 # Visualizzamo la retta di regressione ####
-plot(log(data$Yd), log(data$C), main = "c) La relazione tra reddito e consumi: scatterplot con retta di regressione",
-     xlab = "Reddito disponibile (log, prezzi costanti)",
-     ylab = "Consumo (log, prezzi costanti)")
+plot(delta_Yd/1000, delta_C/1000, main = "b) Variazioni di reddito disponibile e consumo in Italia (2002-2023)",
+     xlab = "Variazione del consumo \n (miliardi di euro del 2010)",
+     ylab = "Variazione del reddito \n (miliardi di euro del 2010)")
 abline(model, col = "red1")
+abline(h=0)
+abline(v=0)
 
-# Calcoliamo i consumi previsti sulla base della regressione ####
-C_e <- c0 + c1*Yd
+# Calcoliamo la variazione nei consumi prevista sulla base della regressione ####
+delta_C_e <- c0 + c1*delta_Yd
 
 # Confrontiamo i consumi previsti dal modello con quelli effettivi ####
-plot(data$Anno, C, main = "d) Consumi effettivi e previsti",
+plot(data$Anno, delta_C/1000, main = "c) Variazione nei consumi effettivi e previsti",
      xlab = "Anno",
-     ylab = "Log")
-lines(data$Anno,C_e,col="purple2")
+     ylab = "Variazioni in miliardi di euro (2010)")
+lines(data$Anno, delta_C_e/1000, col="purple2")
