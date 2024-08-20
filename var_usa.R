a832fcfb3592104583b9f51d1adabe91a8c70c44
Marco Veronese Passarella <marco.veronese.passarella@gmail.com>
2024-08-20 12:29:26 +0200

    Update var_usa.R

--- var_usa.R
+++ var_usa.R
@@ -1,66 +1,122 @@
 # Modello VAR per gli Stati Uniti con dati trimestrali dal 1981 (Q1) al 2017 (Q4)
 
-# Carica i pacchetti necessari ####
+# Carichiamo i pacchetti necessari ####
 library(vars)
 library(tseries)
 library(zoo)
 
-# Carica i dati: pil reale, deflatore del pil (livello dei prezzi) e tasso sui fondi federali ####
+# Importiamo i dati ####
 data <- read.csv("https://www.dropbox.com/scl/fi/u8hsns09gaolfr89nz0yl/Dati_VAR_USA.csv?rlkey=n19p28hpn57kp1s5tpqxnnzer&st=rqhworhu&dl=1")
 
-# Mostra le prime righe dei dati per un controllo rapido ####
+   # Nota: sono 3 serie storiche trimestrali (pil reale, variazione % del deflatore del pil (inflazione) e tasso sui fondi federali)
+
+# Mostriamo le prime righe dei dati per un controllo rapido ####
 head(data)
 
-# Riorganizza i dati ####
+# Cambiamo il formato della colonna "periodo" ####
+data$periodo <- as.yearqtr(data$periodo, format = "%Y Q%q")
+
+# Visualizziamo le 3 serie storiche ####
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
+plot(data$periodo, data$pil,type="l", col=2, ylab="PIL reale", xlab="Trimestre")
+plot(data$periodo, data$p,type="l", col=3, ylab="Tasso di inflazione trimestrale", xlab="Trimestre")
+plot(data$periodo, data$r,type="l", col=4, ylab="Tasso sui federal funds", xlab="Trimestre")
+
+# Esprimiamo anche il tasso di inflazione in termini annuali ####
+data$p_decimal <- data$p / 100
+data$pa <- (1 + data$p_decimal)^4 - 1
+data$pa <- data$pa * 100
+data <- subset(data, select=-p_decimal)
+plot(data$periodo, data$pa,type="l", col="green4", ylab="Tasso di inflazione annuale", xlab="Trimestre")
+
+# Riorganizziamo i dati come serie storiche ####
 data_ts <- ts(data[,], start = c(1981, 1), frequency = 4)
 
-# Controlla se le serie sono stazionarie ####
-adf.test(data_ts[, "gdp"])  # ADF test per il PIL
-adf.test(data_ts[, "p"])    # ADF test per il livello dei prezzi
+# Controlliamo se le serie sono stazionarie ####
+adf.test(data_ts[, "pil"])  # ADF test per il PIL
+adf.test(data_ts[, "pa"])   # ADF test per il tasso di inflazione
 adf.test(data_ts[, "r"])    # ADF test per il tasso sui fondi federali
 
-# Il gdp non è stazionario e forse nemmeno i prezzi, per cui prendi la differenza prima e ripeti il test
-gdp_diff <- diff(data_ts[, "gdp"])
-adf.test(gdp_diff)  # ADF test per dPIL
+# Il PIL non è stazionario, per cui prendiamo la differenza prima e ripetiamo il test
+pil_diff <- diff(data_ts[, "pil"])
+adf.test(pil_diff)  # ADF test per dPIL
 
-p_diff <- diff(data_ts[, "p"])
-adf.test(p_diff)  # ADF test per dPIL
-
-# Modifica la tabella dei dati
-data_ts_diff <- cbind(gdp_diff, p_diff, data_ts[, "r"] )
+# Modifichiamo la tabella dei dati per sostituire il PIL con la sua variazione ####
+data_ts_diff <- cbind(pil_diff, data_ts[, "pa"], data_ts[, "r"] )
 data_ts_diff <- data_ts_diff[-1, ]
-colnames(data_ts_diff) <- c("d_gdp", "d_p", "r")
+colnames(data_ts_diff) <- c("d_pil", "pa", "r")
 
-# Riordina le variabili in ordine decrescente di esogenità
-#data_ts_diff <- data_ts_diff[, c("r", "d_gdp", "d_p")]
+# Riordiniamo le variabili in ordine decrescente di esogenità ####
+# data_ts_diff <- data_ts_diff[, c("r", "d_pil", "pa")]   #sblocca togliendo il cancelletto
 
-# Seleziona i ritardi ("lag") ottimali ####
-lag_selection <- VARselect(data_ts_diff, lag.max = 10, type = "const")
+# Selezioniamo i ritardi ("lag") ottimali ####
+lag_selection <- VARselect(data_ts_diff, lag.max = 10, type = "both")
 print(lag_selection)
 
    # Nota: i test mostrano che i ritardi ottimali sono 2 o 3
 
-# Procedi con la stima del modello VAR ####
-var_model <- VAR(data_ts_diff, p = 2, type = "const")
+# Procediamo con la stima del modello VAR ####
+var_model <- VAR(data_ts_diff, p = 3, type = "both")
 
-# Visualizza i risultati ####
+   # Nota: "both" significa che consideriamo sia un'intercetta costante che un "trend"   
+
+# Visualizziamo i risultati ####
 summary(var_model)
 
-# Analizza la correlazione dei residui ####
+# Analizziamo la correlazione dei residui ####
 serial.test(var_model, lags.pt = 12, type = "PT.asymptotic")
 
    # Nota: il test mostra che è improbabile che i residui siano autocorrelati (buona notizia)
 
-# Crea e visualizza le funzioni di risposta ad impulsi (sul tasso di interesse) ####
-gdp_result <- irf(var_model, impulse = "r", response = "d_gdp", n.ahead = 10)
-plot(gdp_result)
+# Creiamo le funzioni di risposta ad impulso (IRF) sul tasso di interesse ####
+dpil_result <- irf(var_model, impulse = "r", response = "d_pil", n.ahead = 15)
+pa_result <- irf(var_model, impulse = "r", response = "pa", n.ahead = 15)
+r_result <- irf(var_model, impulse = "r", response = "r", n.ahead = 15)
 
-p_result <- irf(var_model, impulse = "r", response = "d_p", n.ahead = 10)
-plot(p_result)
+# Estraiamo i risultati  ####
+dPIL <- dpil_result$irf$r      # Risposta stimata della variazione del PIL
+dPIL_L <- dpil_result$Lower$r  # Limite inferiore dell'intervallo di confidenza
+dPIL_H <- dpil_result$Upper$r  # Limite superiore dell'intervallo di confidenza
 
-r_result <- irf(var_model, impulse = "r", response = "r", n.ahead = 10)
-plot(r_result)
+PA <- pa_result$irf$r          # Risposta stimata del tasso di inflazione
+PA_L <- pa_result$Lower$r      # Limite inferiore dell'intervallo di confidenza
+PA_H <- pa_result$Upper$r      # Limite superiore dell'intervallo di confidenza
 
-# Effettua delle previsioni circa l'andamento futuro delle tre variabili (oltre il 2017) ####
-var_forecast <- predict(var_model, n.ahead = 10)
-plot(var_forecast)
+R <- r_result$irf$r            # Risposta stimata del tasso di interesse
+R_L <- r_result$Lower$r        # Limite inferiore dell'intervallo di confidenza
+R_H <- r_result$Upper$r        # Limite superiore dell'intervallo di confidenza
+
+# Visualizziamo le funzioni di risposta ad impulso ####
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
+
+# IRF della variazione del PIL
+plot(dPIL, type="l", lwd = 2, main = "Risposta all'impulso ortogonale da R",
+     ylab = "Variazione del PIL", xlab="Trimestre \n Intervallo di confidenza bootstrap al 95%, 100 iterazioni",
+     ylim=range(min(dPIL_L),max(dPIL_H)))
+lines(dPIL_L, col=2, lty=2)
+lines(dPIL_H, col=2, lty=2)
+abline(h=0)
+
+# IRF del tasso di inflazione
+plot(PA, type="l", lwd = 2, main = "Risposta all'impulso ortogonale da R",
+     ylab = "Tasso di inflazione", xlab="Trimestre \n Intervallo di confidenza bootstrap al 95%, 100 iterazioni",
+     ylim=range(min(PA_L),max(PA_H)))
+lines(PA_L, col=3, lty=2)
+lines(PA_H, col=3, lty=2)
+abline(h=0)
+
+# IRF del tasso di interesse
+plot(R, type="l", lwd = 2, main = "Risposta all'impulso ortogonale da R",
+     ylab = "Tasso di interesse", xlab="Trimestre \n Intervallo di confidenza bootstrap al 95%, 100 iterazioni",
+     ylim=range(min(R_L),max(R_H)))
+lines(R_L, col=4, lty=2)
+lines(R_H, col=4, lty=2)
+abline(h=0)
+   
+   # Nota: alternativamente, possiamo limitarci ad utilizzare il comando plot(pil_result), ecc.
+
+# Effettuiamo delle previsioni circa l'andamento futuro delle variabili ####
+# Consideriamo 15 trimestri oltre il 2017
+# var_forecast <- predict(var_model, n.ahead = 15) #sblocca togliendo il cancelletto
+# plot(var_forecast)                               #sblocca togliendo il cancelletto
+  
